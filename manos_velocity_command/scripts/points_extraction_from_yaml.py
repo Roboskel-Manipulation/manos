f63f3e357de4ef5e2dcc18697d3465216f3d5486
Thanasists <thtsitos@gmail.com>
2020-03-06 19:07:16 +0200

    minor changes

--- manos_velocity_command/scripts/points_extraction_from_yaml.py
+++ manos_velocity_command/scripts/points_extraction_from_yaml.py
@@ -5,53 +5,78 @@ import rospy
 from trajectory_smoothing_msg.msg import *
 from geometry_msgs.msg import Point
 from scipy.spatial import distance
+import matplotlib.pyplot as plt
+import numpy as np
+from statistics import median
 
 def main():
 	rospy.init_node("points_extraction_from_yaml")
 	pub = rospy.Publisher("/trajectory_points", SmoothRWristCoordsWithRespectToBase, queue_size=10)
-	print sys.argv[1]
-	times = list()
+	flag = sys.argv[2]
 	points = SmoothRWristCoordsWithRespectToBase()
+	xRaw, yRaw, zRaw = [], [], []
+	times = []
+	
+	boundX = 0.012
+	boundY = 0.03
+
 	file = open(sys.argv[1], 'r')
 	fl = file.readlines()
+	if flag == '0':
+		for j in xrange(len(fl)):
+			if 'x' in fl[j]:
+				point = Point()
+				point.x = float(fl[j][7:])
+				point.y = float(fl[j+1][7:])
+				point.z = float(fl[j+2][7:])
+				points.points.append(point)
+	else:
+		for j in xrange(len(fl)):
+			if "RWrist" in fl[j]:
+				y = float(fl[j+10][11:])
+				x = float(fl[j+9][11:])
+				z = float(fl[j+11][11:])
+				time = int(fl[j+5][16:])
+				if x != 0 and y != 0 and z!=0:
+					if len(xRaw) >= 1:
+						if (abs(xRaw[-1] - x) > 0.1 or abs(yRaw[-1] - y) > 0.1):
+							continue
+						else:
+							xRaw.append(x)
+							yRaw.append(y)
+							zRaw.append(z)
+					else:
+						xRaw.append(x)
+						yRaw.append(y)
+						zRaw.append(z)
+				if len(times) >= 1 and times[-1] <= time:
+					times.append(time)
+				elif len(times) == 0:
+					times.append(time)
+				else:
+					break
+	
+		for i in xrange(15, len(xRaw)-1):
+			if abs(xRaw[i] - median(xRaw[0:i])) > boundX or abs(yRaw[i] - median(yRaw[0:i])) > boundY:
+				break
 
-	for j in xrange(len(fl)):
-		if 'x' in fl[j]:
+		for j in xrange(len(xRaw)-15, 1, -1):
+			if abs(xRaw[j] - median(xRaw[j:len(xRaw)])) > boundX or abs(yRaw[j] - median(yRaw[j:len(yRaw)])) > boundY:
+				break
+
+		xRaw = xRaw[i-3:j+3]
+		yRaw = yRaw[i-3:j+3]
+		zRaw = zRaw[i-3:j+3]
+
+		for i in xrange(len(xRaw)):
 			point = Point()
-			point.x = float(fl[j][7:])
-			point.y = float(fl[j+1][7:])
-			point.z = float(fl[j+2][7:])
+			point.x = xRaw[i]
+			point.y = yRaw[i]
+			point.z = zRaw[i]
 			points.points.append(point)
-
-	# for j in xrange(len(fl)):
-	# 	if "RWrist" in fl[j]:
-	# 		point = Point()
-	# 		point.x = float(fl[j+9][11:])
-	# 		point.y = float(fl[j+10][11:])
-	# 		point.z = float(fl[j+11][11:])
-	# 		time = int(float(fl[j+5][16:]))
-	# 		if (point.x == 0 and point.y == 0 and point.z == 0):
-	# 			continue
-	# 		if len(points.points) >= 1:
-	# 			if times[-1] > time:
-	# 				# print times[-1], time
-	# 				break
-	# 			if (abs(points.points[-1].x - point.x) > 0.1 or abs(points.points[-1].y - point.y) > 0.1 or abs(points.points[-1].z - point.z) > 0.1):
-	# 				continue
-	# 			current_point = [point.x, point.y, point.z]
-	# 			last_point = [points.points[-1].x, points.points[-1].y, points.points[-1].z]
-	# 			if distance.euclidean(current_point, last_point) < 0.015:
-	# 				continue
-	# 			points.points.append(point)
-	# 			times.append(time)
-	# 		else:
-	# 			points.points.append(point)
-	# 			times.append(time)
 
 	rospy.sleep(0.2)
 	pub.publish(points)
 	print "Published the points"
-	# rospy.spin()
-
 
 main()
--- manos_velocity_command/src/motion_replication.cpp
+++ manos_velocity_command/src/motion_replication.cpp
@@ -3,7 +3,9 @@
 #include <trajectory_smoothing_msg/SmoothRWristCoordsWithRespectToBase.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Twist.h>
+
 #include <cmath>
+#include <vector>
 
 #include <signal.h>
 
@@ -18,14 +20,27 @@ geometry_msgs::TwistPtr zero_vel = boost::make_shared<geometry_msgs::Twist>();
 
 trajectory_smoothing_msg::SmoothRWristCoordsWithRespectToBase points;
 
-float dt, Dt, xOffset, yOffset, zOffset;
-bool flag = false, init_flag = false;
+std::shared_ptr<std::vector<double>> v1 = std::make_shared<std::vector<double>>();
+std::shared_ptr<std::vector<double>> v2 = std::make_shared<std::vector<double>>();
 
+
+float dt, init_gain, Dt, xOffset, yOffset, zOffset;
+bool flag = false, init_flag = false;
+bool final_flag = false;
 
 void signal_handler(int sig){
 	std::cout << "Recieved signal " << sig << std::endl;
 	std::cout << "Gonna exit..." << std::endl;
 	exit (-1);
+}
+
+
+double euclidean_distance (std::shared_ptr<std::vector<double>> v1, std::shared_ptr<std::vector<double>> v2){
+	double temp = 0;
+	for (short int i=0; i<v1->size(); i++){
+		temp += pow((v1->at(i) - v2->at(i)), 2);
+	}
+	return sqrt(temp);
 }
 
 
@@ -38,6 +53,7 @@ void vel_callback (trajectory_smoothing_msg::SmoothRWristCoordsWithRespectToBase
 		
 		points.points.push_back(*temp_point);
 	}
+
 	std::cout << points.points.size() << std::endl;
 	while (not init_flag){
 		// std::cout << *ee_position << std::endl;
@@ -45,7 +61,7 @@ void vel_callback (trajectory_smoothing_msg::SmoothRWristCoordsWithRespectToBase
 		vel->linear.y = (points.points[0].y - ee_position->y);
 		vel->linear.z = (points.points[0].z - ee_position->z);
 		vel_pub.publish(vel);
-		ros::Duration(dt).sleep();
+		ros::Duration(0.2).sleep();
 		if (abs(points.points[0].x - ee_position->x) < 0.005 and abs(points.points[0].y - ee_position->y) < 0.005 and abs(points.points[0].z - ee_position->z) < 0.005){
 			ROS_INFO("Reached initial position");
 			init_flag = true;
@@ -54,12 +70,39 @@ void vel_callback (trajectory_smoothing_msg::SmoothRWristCoordsWithRespectToBase
 	
 	if (init_flag){
 		ros::Duration(0.5).sleep();
-		for (short int i=0; i<points.points.size(); i++){
+		for (short int i=1; i<points.points.size(); i++){
+			v1->at(0) = points.points[i-1].x;
+			v1->at(1) = points.points[i-1].y;
+			v1->at(2) = points.points[i-1].z;
+			v2->at(0) = points.points[i].x;
+			v2->at(1) = points.points[i].y;
+			v2->at(2) = points.points[i].z;
+			// Dt = init_gain*euclidean_distance(v1, v2);
+			// if (Dt == 0){
+			// 	Dt = 1;
+			// }
+			std::cout << Dt << std::endl;
 			vel->linear.x = (points.points[i].x - ee_position->x)/Dt;
 			vel->linear.y = (points.points[i].y - ee_position->y)/Dt;
 			vel->linear.z = (points.points[i].z - ee_position->z)/Dt;
 			vel_pub.publish(vel);
-			ros::Duration(Dt).sleep();
+			if (i == points.points.size()-1){
+				while (not final_flag){
+					// std::cout << *ee_position << std::endl;
+					vel->linear.x = (points.points[i].x - ee_position->x);
+					vel->linear.y = (points.points[i].y - ee_position->y);
+					vel->linear.z = (points.points[i].z - ee_position->z);
+					vel_pub.publish(vel);
+					ros::Duration(0.2).sleep();
+					if (abs(points.points[i].x - ee_position->x) < 0.005 and abs(points.points[i].y - ee_position->y) < 0.005 and abs(points.points[i].z - ee_position->z) < 0.005){
+						ROS_INFO("Reached final point");
+						final_flag = true;
+					}		
+				}
+			}
+			else{
+				ros::Duration(dt).sleep();
+			}
 		}
 		ROS_INFO("Published all velocities");
 		vel_pub.publish(*zero_vel);
@@ -88,6 +131,10 @@ int main(int argc, char**argv){
 	nh.param("motion_replication/zOffset", zOffset, 0.0f);
 	nh.param("motion_replication/dt", dt, 0.0f);
 	nh.param("motion_replication/Dt", Dt, 0.0f);
+
+	nh.param("motion_replication/init_gain", init_gain, 0.0f);
+	v1->resize(3);
+	v2->resize(3);
 
 
 	zero_vel->linear.x = 0;
